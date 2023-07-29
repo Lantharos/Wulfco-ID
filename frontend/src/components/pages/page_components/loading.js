30d07d267bc9feae4e7eaa64121350d82b5da0e4
Viktor Blažarić <viktorblazaric2@gmail.com>
2023-07-30 01:49:05 +0200

    Updated the loading screen

--- frontend/src/components/pages/page_components/loading.js
+++ frontend/src/components/pages/page_components/loading.js
@@ -1,18 +1,14 @@
-import React from 'react'
-
-import './loading.css'
-import { motion } from 'framer-motion'
+import React from 'react';
+import './loading.css';
+import { motion } from 'framer-motion';
+import LightningStrike from './LightningStrike'; // Import the LightningStrike component
 
 const Loading = (props) => {
     return (
         <div className="coming-soon-container">
-            <div>
-                <motion.a animate={{opacity: [1, 0.5, 0], scale: [3, 3.2, 3],}} transition={{ duration: 0.6, repeat: Infinity }} style={{ color: "#ffffff", fontFamily: "Roboto", display: "inline-flex", marginRight: "0.5em", fontSize: "24px", cursor: "default", userSelect: "none" }}>.</motion.a>
-                <motion.a animate={{opacity: [0, 1, 0.5], scale: [3, 3, 3.2],}} transition={{ duration: 0.6, repeat: Infinity, delay: 0.2 }} style={{ color: "#ffffff", fontFamily: "Roboto", display: "inline-flex", marginRight: "0.5em", fontSize: "24px", cursor: "default", userSelect: "none" }}>.</motion.a>
-                <motion.a animate={{opacity: [0.5, 0, 1], scale: [3.2, 3, 3],}} transition={{ duration: 0.6, repeat: Infinity, delay: 0.4 }} style={{ color: "#ffffff", fontFamily: "Roboto", display: "inline-flex", marginRight: "0.5em", fontSize: "24px", cursor: "default", userSelect: "none" }}>.</motion.a>
-            </div>
+            <LightningStrike color="#FF4444" /> {/* Use any color you like */}
         </div>
     );
-}
+};
 
-export default Loading
+export default Loading;
